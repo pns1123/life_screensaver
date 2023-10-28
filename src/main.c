ef1d17aec446dd12126b96c9bf735b88ccf00b2d
Philipp Schroeppel <69214052+pns1123@users.noreply.github.com>
2023-10-28 18:00:03 +0200

    feat: do not draw full rectangle, change pattern to gosperglidergun

--- src/main.c
+++ src/main.c
@@ -8,9 +8,6 @@
 #include "life.c"
 #include "parser.c"
 
-//Display *dpy;
-//Window root;
-//GC g;
 
 #define BOX_LENGTH 10
 #define NOF_HORIZONTAL_BOXES 192
@@ -22,7 +19,7 @@ void display_grid(bool *current_gen, unsigned int grid_len,
 		  Display *dpy, Window root, GC g,
 		  unsigned int *x_coordinates,
 		  unsigned int *y_coordinates){
-    /* draw a square */
+    // draw a square
     unsigned int x_index, y_index;
     for(unsigned int grid_index=0; grid_index<grid_len; ++grid_index){
        x_index = grid_index2x_index(grid_index, nof_horizontal_boxes, nof_vertical_boxes);
@@ -30,13 +27,14 @@ void display_grid(bool *current_gen, unsigned int grid_len,
 
        if( current_gen[grid_index] ){
           XFillRectangle (dpy, root, g, x_coordinates[x_index], y_coordinates[y_index], 
-      	            box_length, box_length);
+      	            box_length-1, box_length-1);
 
        }
     }
 }
 
 int main(){
+
 	/* open the display (connect to the X server) */
 	Display *dpy = XOpenDisplay (getenv ("DISPLAY"));
 	
@@ -53,14 +51,9 @@ int main(){
 	/* set foreground color */
 	XSetForeground(dpy, g, WhitePixelOfScreen(DefaultScreenOfDisplay(dpy)) );
 	
-	/* translation: coordinates to life grid */
-	//unsigned int box_length = 10;
-	//unsigned int nof_vertical_boxes = (/*wa.height*/ 1080 / box_length);
-	//unsigned int nof_horizontal_boxes = (/*wa.width*/ 1920 / box_length);
-	//unsigned int nof_boxes = nof_horizontal_boxes * nof_vertical_boxes;
 	bool grid_A[NOF_HORIZONTAL_BOXES * NOF_VERTICAL_BOXES] = { false };
 
-	char *filename = "/home/philipp/.life_patterns/glider.rle";
+	char *filename = "/home/philipp/.life_patterns/gosperglidergun.rle";
 
 	struct GridSize pattern_size = parse_grid_size(filename);                 
                                                                                
@@ -71,7 +64,6 @@ int main(){
 	                pattern_arr, pattern_size.horizontal, pattern_size.vertical);
 	
 	unsigned int generation_counter = 0;
-	//initialize_grid_A(grid_A, nof_boxes);
 
 	bool grid_B[NOF_HORIZONTAL_BOXES * NOF_VERTICAL_BOXES];
 
@@ -83,11 +75,6 @@ int main(){
 
 	while (1){
 
-    		//printf("%zu (%zu, %zu) - (%zu, %zu)  \n", 
-           	//       grid_index, x_index, y_index, x_coordinates[x_index], y_coordinates[y_index]);
-    
-    
-    
     		display_grid(grid_A, NOF_HORIZONTAL_BOXES * NOF_VERTICAL_BOXES, NOF_HORIZONTAL_BOXES, NOF_VERTICAL_BOXES, BOX_LENGTH, dpy, root, g, x_coordinates, y_coordinates);
 		update_grid(grid_A, grid_B, NOF_HORIZONTAL_BOXES * NOF_VERTICAL_BOXES, NOF_HORIZONTAL_BOXES, NOF_VERTICAL_BOXES);
 
